4bf6ce042eae5278f01488458d7df6b7f162d7e8
zhangsheng <zhangsheng@xyd.cn>
2026-05-07 15:45:51 +0800

    refactor: 自动推导搜索参数 + 精简代码 + 修正回测报告

--- optimize_thresholds.py
+++ optimize_thresholds.py
@@ -5,32 +5,11 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from main import GtechQuantStrategy
 
 if __name__ == "__main__":
-    # # 创建策略实例
-    # strategy = GtechQuantStrategy(stock_code='sz000727', init_cash=100000)
-    #
-    # # 运行阈值优化
-    # # start_date 和 end_date -> 回测的时间区间
-    # # buy_range = (2.4, 2.6) -> 买入窗口，要在该窗口值中筛选出最好的买入值
-    # # sell_range = (2.7, 2.9) -> 卖出窗口，要在该窗口值中筛选出最好的卖出值
-    # # step = 0.05 -> 梯度下降的步长
-    # best_buy_threshold, best_sell_threshold, best_return = strategy.optimize_thresholds(
-    #     start_date='2025-01-01',
-    #     end_date='2026-05-07',
-    #     buy_range=(2.2, 2.8),
-    #     sell_range=(2.6, 3.2),
-    #     step=0.01
-    # )
+    strategy = GtechQuantStrategy(stock_code='sz000727', init_cash=100000)
 
-    # 创建策略实例
-    strategy = GtechQuantStrategy(stock_code='sz002415', init_cash=100000)
-
-    # 运行阈值优化
     best_buy_threshold, best_sell_threshold, best_return = strategy.optimize_thresholds(
         start_date='2025-01-01',
-        end_date='2026-05-07',
-        buy_range=(28, 36),
-        sell_range=(35, 40),
-        step=0.05
+        end_date='2026-05-07'
     )
 
     print(f"最终优化结果:")
